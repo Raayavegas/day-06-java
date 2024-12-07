6a7040918c5fab16b57ed290a51b75976940d19e
Raayavegas <veeralarithika@gmail.com>
2024-12-07 14:14:06 +0530

    Add files via upload

---
+++ break.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class A 
+{
+    public static void main(String args[])
+    {
+        for(int i=0;i<5;i++)
+        {
+            if(i==2)
+            {
+                break;
+            }
+            System.out.println(i);
+        }
+        for(int i=0;i<5;i++)
+        {
+            if(i==2)
+            {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
