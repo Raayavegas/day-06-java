4fc9515541663f702f2f87a1c4111a63d9ce0eb1
Raayavegas <veeralarithika@gmail.com>
2024-12-07 16:21:41 +0530

    Add files via upload

---
+++ singlearray.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class A 
+{
+    public static void main(String args[])
+    {
+    int a[]={11,18,22,64,181};
+    System.out.println(a[0]);
+    System.out.println(a[3]);
+    a[2]=445;
+    a[3]=884;
+    System.out.println(Arrays.toString(a));    
+    }
+}
